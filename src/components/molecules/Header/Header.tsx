60fe5343ac72a98c5f73b7e6f41ac92418c428ad
Mohit Raj Purohit <mohpuroh@publicisgroupe.net>
2023-08-19 23:04:54 +0530

    adds header

--- src/components/molecules/Header/Header.tsx
+++ src/components/molecules/Header/Header.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import { CiSearch, CiShoppingCart } from "react-icons/ci";
+import {CiSearch, CiShoppingCart} from 'react-icons/ci';
 import viteLogoDark from '../../../assets/logo-dark.png';
 import './index.css';
-import { Link } from 'react-router-dom';
-import { HOME_PAGE_PATH } from '../../../constants';
+import {Link} from 'react-router-dom';
+import {HOME_PAGE_PATH} from '../../../constants';
 // import { useDispatch, useSelector } from 'react-redux';
 // import { addItem } from '../../../redux/slices/CartSlice';
 // import {AppState} from '../../../redux/store/store';
@@ -21,15 +21,42 @@ const Header: React.FC = () => {
 	// 	dispatch(clearCart());
 	// }
 	return (
-		<header className={`header bg-white border-b border-gray-200 sticky top-0 dark:bg-slate-800`} >
+		<header
+			className={`header bg-white border-b border-gray-200 sticky top-0 dark:bg-slate-800`}
+		>
 			<div className="v-container mx-auto flex justify-between py-3">
-				<Link to={HOME_PAGE_PATH} className='flex items-center justify-center'>
+				<Link
+					to={HOME_PAGE_PATH}
+					className="flex items-center justify-center"
+				>
 					<img src={viteLogoDark} className="w-28 block" alt="logo" />
 				</Link>
-				<button type='button' className='ml-auto mr-3'>
+				<nav className="ml-10">
+					<div className="flex flex-wrap items-center justify-between mx-auto p-4">
+						<ul className="flex flex-raw ">
+							<li>
+								<a href="#" className="block py-2 pl-3 pr-4">
+									Categories
+								</a>
+							</li>
+							<li>
+								<a href="#" className="block py-2 pl-3 pr-4">
+									Products
+								</a>
+							</li>
+							<li>
+								<a href="#" className="block py-2 pl-3 pr-4">
+									Contact
+								</a>
+							</li>
+						</ul>
+					</div>
+				</nav>
+
+				<button type="button" className="ml-auto mr-3">
 					<CiSearch size={25} />
 				</button>
-				<button type='button'>
+				<button type="button">
 					<CiShoppingCart size={25} />
 				</button>
 			</div>
