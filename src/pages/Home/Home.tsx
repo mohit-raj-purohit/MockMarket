f9a414efbaab9811fd7d9b60c43a824cbe1219e5
Mohit Raj Purohit <mohpuroh@publicisgroupe.net>
2023-08-19 23:16:20 +0530

    adds header nav

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -105,7 +105,7 @@ const Home = () => {
 					</div>
 				</div>
 			</div>
-			<div className="mt-16">
+			<div className="mt-11">
 				<h3 className="text-gray-600 text-2xl font-medium">Fashions</h3>
 				<div className="grid gap-6 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 mt-6">
 					<div className="w-full max-w-sm mx-auto rounded-md shadow-md overflow-hidden">
