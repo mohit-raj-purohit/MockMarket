ba936de44fd3b918264c94048165c705a540ad87
Mohit Raj Purohit <mohpuroh@publicisgroupe.net>
2023-08-20 01:42:05 +0530

    header nav changes

--- src/components/molecules/Header/Header.tsx
+++ src/components/molecules/Header/Header.tsx
@@ -1,30 +1,23 @@
-import React from 'react';
-import {CiSearch, CiShoppingCart} from 'react-icons/ci';
-import viteLogoDark from '../../../assets/logo-dark.png';
-import './index.css';
+import React, {useState} from 'react';
+import {CiMenuBurger, CiSearch, CiShoppingCart} from 'react-icons/ci';
 import {Link} from 'react-router-dom';
 import {HOME_PAGE_PATH} from '../../../constants';
 import {MAIN_NAVIGATION} from '../../../services/navigation.service';
-// import { useDispatch, useSelector } from 'react-redux';
-// import { addItem } from '../../../redux/slices/CartSlice';
-// import {AppState} from '../../../redux/store/store';
-// import { clearCart } from '../../../redux/actions';
+import viteLogoDark from '../../../assets/logo-dark.png';
+import './index.css';
 
 const Header: React.FC = () => {
-	// const dispatch = useDispatch();
-	// const data = useSelector((state: AppState) => {
-	// 	return state.cart;
-	// });
-	// const handleHamburgerClick = () => {
-	// 	dispatch(addItem({id: 1}));
-	// }
-	// const handleEmptyCart = () => {
-	// 	dispatch(clearCart());
-	// }
+	const [isNavVisible, setIsNavVisible] = useState(false);
+	const toggleNav = () => {
+		setIsNavVisible((prevState) => !prevState);
+	};
+	const hideNav = () => {
+		setTimeout(() => {
+			setIsNavVisible(false);
+		}, 300);
+	};
 	return (
-		<header
-			className={`header bg-white border-b border-gray-200 sticky top-0 dark:bg-slate-800`}
-		>
+		<header className="header bg-white border-b border-gray-200 sticky top-0">
 			<div className="v-container mx-auto flex justify-between py-3">
 				<Link
 					to={HOME_PAGE_PATH}
@@ -33,16 +26,19 @@ const Header: React.FC = () => {
 					<img src={viteLogoDark} className="w-28 block" alt="logo" />
 				</Link>
 				{MAIN_NAVIGATION.length && (
-					<nav className="ml-10">
-						<div className="flex flex-wrap items-center justify-between mx-auto p-4">
-							<ul className="flex-raw hidden md:flex ">
+					<nav
+						className={`main-navigation ${
+							!isNavVisible && 'hidden'
+						} md:flex absolute bg-white w-full left-0 top-full border-b border-gray-200 md:static md:ml-10 md:w-auto md:border-b-0`}
+					>
+						<div className="flex flex-wrap items-center justify-between mx-auto">
+							<ul className="flex-row md:flex w-full md:w-auto py-3">
 								{MAIN_NAVIGATION.map(({name, path}, index) => {
 									return (
-										<li>
+										<li key={index} className="w-full md:w-auto">
 											<Link
 												to={path}
-												key={index}
-												className="block py-2 pl-3 pr-4"
+												className="block py-2 px-5 md:px-3"
 											>
 												{name}
 											</Link>
@@ -57,8 +53,11 @@ const Header: React.FC = () => {
 				<button type="button" className="ml-auto mr-3">
 					<CiSearch size={25} />
 				</button>
-				<button type="button">
+				<button type="button" className="mr-3 md:mr-0">
 					<CiShoppingCart size={25} />
+				</button>
+				<button type="button" className="md:hidden" onClick={toggleNav} onBlur={hideNav}>
+					<CiMenuBurger size={25} />
 				</button>
 			</div>
 		</header>
