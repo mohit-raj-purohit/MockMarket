edd8a4957a338c17c87efe39d8ddce408983292b
Mohit Raj Purohit <mohpuroh@publicisgroupe.net>
2023-08-28 01:45:03 +0530

    ui builds

--- src/constants/api.ts
+++ src/constants/api.ts
@@ -1,4 +1,4 @@
-export const API_URL = 'https://unusual-hosiery-jay.cyclic.cloud';
-export const CATEGORIES_URL = '/categories';
-export const CATEGORY_URL = '/products?category=';
-export const PRODUCTS_URL = '/products';
+export const API_URL:string = 'https://unusual-hosiery-jay.cyclic.cloud';
+export const CATEGORIES_URL:string = '/categories';
+export const CATEGORY_URL:string = '/products?category=';
+export const PRODUCTS_URL:string = '/products';
