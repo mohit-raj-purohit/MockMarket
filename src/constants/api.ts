a163e0d4bacdc069dd4eb1b4a260eabe2bbc238c
Mohit Raj Purohit <mohpuroh@publicisgroupe.net>
2023-08-27 22:19:01 +0530

    adds products

--- src/constants/api.ts
+++ src/constants/api.ts
@@ -1,4 +1,4 @@
 export const API_URL = 'https://unusual-hosiery-jay.cyclic.cloud';
 export const CATEGORIES_URL = '/categories';
-export const CATEGORY_URL = '/products/category';
+export const CATEGORY_URL = '/products?category=';
 export const PRODUCTS_URL = '/products';
--- src/services/products.ts
+++ src/services/products.ts
@@ -2,7 +2,7 @@ import {CATEGORY_URL, PRODUCTS_URL} from '../constants/api';
 import {apiInstance} from './apiInstance';
 
 export const fetchProducts = async (category?: string) => {
-	const url = category ? `${CATEGORY_URL}/${category}` : PRODUCTS_URL;
+	const url = category ? `${CATEGORY_URL}${category}` : PRODUCTS_URL;
 	const response = await apiInstance.get(url);
 	return response.data;
 };
